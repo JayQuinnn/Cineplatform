12e806044cb4dc1031031186623d7d63e47bb295
Jochem Crab <45558061+JayQuinnn@users.noreply.github.com>
2024-05-16 15:42:40 +0200

    fix: updated resolveHandler to work with API

--- back-end/resolveHandler.py
+++ back-end/resolveHandler.py
@@ -1,8 +1,3 @@
-# TODO:
-#     - FIX RESOLUTION
-#     - FIX ENCODING -> mp4
-
-#!/usr/bin/env python
 import DaVinciResolveScript as dvr_script
 import sys
 # 1. Assign Resolve
@@ -12,52 +7,26 @@ projectManager = resolve.GetProjectManager ()
 # 3. Setup a Project
 pname = input('Insert the name for this project ')
 project = projectManager.CreateProject (pname)
-
-print(project.GetSetting('timelineResolutionWidth'))
-print(project.GetSetting('timelineResolutionHeight'))
 # 4. Object of MediaPool is gotten from project.
 mediaPool = project.GetMediaPool()
 # 5. Media Storage
-mediaStorage = resolve. GetMediaStorage()
+mediaStorage = resolve.GetMediaStorage()
 # 5.1. Import Media
-clip = mediaPool. ImportMedia( ['/Users/jochem/Desktop/IMG_0612.MOV'])
-print('CLIPRESOLUTION:')
-print(clip[0].GetClipProperty('Resolution'))
+clip = mediaPool.ImportMedia( ['/Users/jochem/Documents/GitHub/Cineplatform/Cineplatform/back-end/uploads/JochemCrab_InteractiveMotion_Les1.mov'])
 clipResolution = clip[0].GetClipProperty('Resolution')
-
-
+#Set timeline to clip resolution
 before_x, after_x = clipResolution.split('x')
 sourceWidth = int(before_x)
-print('Source Width: ' + str(sourceWidth))
 sourceHeight = int(after_x)
-print('Source Height: ' + str(sourceHeight))
-
-
+#Hotfix 1 - Resolution
 project.SetSetting('timelineResolutionWidth', str(sourceWidth))
 project.SetSetting('timelineResolutionHeight', str(sourceHeight))
-
-
-# 7. Create an empty timeline
-# tl = mediaPool. CreateEmptyTimeline('t1')
-# # 8. Try to get a current timeline
-# timeline = project.GetCurrentTimeline()
-# # if not, then try to get the first timeline and set it to current
-# if not timeline:
-#     if project. GetTimelineCount() > 0:
-#         timeline = project.GetTimelineByIndex(1)
-#         project. SetCurrentTimeline(timeline)
-# # 9. Append Clips to the timeline
-# appended = mediaPool.AppendToTimeline(clip)
-
 timeline = mediaPool.CreateTimelineFromClips('timeline',clip)
-
-print(timeline.GetSetting())
+#Hotfix 2 - Force Resolution settings
 timeline.SetSetting('timelineOutputResolutionHeight', str(sourceHeight))
 timeline.SetSetting('timelineOutputResolutionWidth',str(sourceWidth))
 timeline.SetSetting('timelineResolutionHeight',str(sourceHeight))
 timeline.SetSetting('timelineResolutionWidth',str(sourceWidth))
-print("-------------")
-print(timeline.GetSetting())
 # Set the render settings
 Settings = {
     'SelectAllFrames': True,
@@ -66,15 +35,10 @@ Settings = {
     "VideoQuality": 0,
     "ExportVideo": True,
     "ExportAudio": True,
-    # "FormatWidth": videoWidth,
-    # "FormatHeight": videoHeight,
 }
 project.SetRenderSettings(Settings)
-
 # Render the timeline
-
 render_job = project.AddRenderJob()
 # project.StartRendering()
-
 
 
